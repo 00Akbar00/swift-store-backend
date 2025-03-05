36200f0d5fd0a0368e64d2d32ecb05cef824039d
00Akbar00 <ma341604@gmail.com>
2025-03-05 22:49:50 +0500

    DataBase Connection Made

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { connectDB } = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
